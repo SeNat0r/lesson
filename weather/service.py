80fe64f5b9230513843d4428362572e4768cad68
SeNat0r <sergey1993@yandex.ru>
2017-04-07 20:21:20 +0300

    +1

--- weather/service.py
+++ weather/service.py
@@ -1,5 +1,14 @@
 import argparse
+from io import StringIO
+from datetime import datetime
+from os.path import exists as file_exists
+from time import sleep
+from urllib.parse import urljoin
+import socket
+
 import requests
+from lxml import etree
+
 
 # requests
 # lxml  Xpath => лучше для xml
@@ -17,7 +26,7 @@ class Page(object):
     def __call__(self, refresh=False):
         """Вызывается, когда объект используют как функцию"""
         if refresh or self.__response is None:
-            self.__response = requests.get(self.__url, self.__params, self.__kwargs)
+            self.__response = requests.get(self.__url, self.__params, **self.__kwargs)
         return self.__response
 
 
@@ -38,6 +47,29 @@ class Finder(object):
         """ВЫполняет поиск всех элесентов на странице с заданным CSS селектором"""
         return self.context.cssselect(css_selector)
 
+    @property
+    def context(self): # getter
+        if self.__context is None:
+            return self.tree.getroot() # получить коренаой элемент
+        return self.__context
+
+    @context.setter
+    def context(self, css_selector):  #setter
+        self.__context = self(css_selector)
+
+    @context.deleter
+    def context(self):
+        self.__context = None
+
+    @property
+    def tree(self):
+        """Дерево веб документа"""
+        if self.__tree is None:
+            parser = etree.HTMLParser()
+            self.__tree = etree.parse(StringIO(self.__page().text), parser)
+        return self.__tree
+
+
 
 class WeatherGrabber(object):
     """Граббер и парсер данных с сервиса Яндекс-Погода"""
@@ -50,8 +82,10 @@ class WeatherService(object):
         print(search)
 
     def listen(self, ip, port):
-        print(ip, port)
+        founder = Finder(Page('https://yandex.ru/pogoda/saint-petersburg'))
 
+        temperature = founder('.current-weather__thermometer.current-weather__thermometer_type_now')
+        print(temperature.text)
 
 if __name__ == '__main__':
     service = WeatherService()
